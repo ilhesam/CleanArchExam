cc4207264f5f65e4b7438e5d2c42711724da2e74
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-25 19:54:00 +0430

    Add post entity.

---
+++ Src/Domain/Post/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Post : Entity
+    {
+        public string Subject { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageAddress { get; set; }
+    }
+}
---
+++ Src/Infrastructure/Persistence/Configurations/Post/PostConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations.Post
+{
+    public class PostConfiguration : EntityConfiguration<Domain.Post>
+    {
+        public override void Configure(EntityTypeBuilder<Domain.Post> builder)
+        {
+            builder?.Property(p => p.Subject)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder?.Property(p => p.Description)
+                .IsRequired()
+                .HasMaxLength(1500);
+
+            builder?.Property(p => p.ImageAddress)
+                .HasMaxLength(1500);
+
+            base.Configure(builder);
+        }
+    }
+}
