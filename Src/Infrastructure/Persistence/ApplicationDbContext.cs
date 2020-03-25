81b304f0a67e9fd7a960ef0fbb628461e539ca9e
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-25 20:08:37 +0430

    Add database context.

---
+++ Src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+using Infrastructure.Persistence.Configurations.Post;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        #region Constructors
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public virtual DbSet<Post> Posts { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder?.ApplyConfiguration<Post>(new PostConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #endregion
+    }
+}
