786af28eeb2a6e175a25caa8f666ad881999b460
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-25 20:25:41 +0430

    Add dependency injection file.

---
+++ Src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
