d677cc9f8c08802516f3db9ca8461f0308aee225
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-25 19:40:08 +0430

    Add base entity.

---
+++ Src/Domain/Base/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
---
+++ Src/Infrastructure/Persistence/Configurations/Base/EntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations.Base
+{
+    public class EntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : Entity
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder?.HasKey(e => e.Id);
+
+            builder?.Property(e => e.CreateDateTime)
+                .IsRequired();
+        }
+    }
+}
