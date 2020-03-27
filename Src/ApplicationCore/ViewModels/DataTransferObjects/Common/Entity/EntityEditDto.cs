faa6cb7fc48b70f15b37788a4af0623ec0b3d167
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-27 17:30:20 +0430

    Add entity data transfer objects

---
+++ Src/ApplicationCore/ViewModels/DataTransferObjects/Common/Entity/EntityEditDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.ViewModels.DataTransferObjects.Common.Entity
+{
+    public class EntityEditDto
+    {
+    }
+}
---
+++ Src/ApplicationCore/ViewModels/DataTransferObjects/Common/Entity/EntityGetDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.ViewModels.DataTransferObjects.Common.Entity
+{
+    public class EntityGetDto
+    {
+        public int Id { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+    }
+}
