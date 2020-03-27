4dc8419832c92f59689f2701e29554c8be16bbe6
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-27 17:32:20 +0430

    Modify previous commit (faa6cb7fc48b70f15b37788a4af0623ec0b3d167)

--- Src/ApplicationCore/ViewModels/DataTransferObjects/Common/Entity/EntityEditDto.cs
+++ Src/ApplicationCore/ViewModels/DataTransferObjects/Common/Entity/EntityEditDto.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ApplicationCore.ViewModels.DataTransferObjects.Common.Entity
+namespace ApplicationCore.ViewModels.DataTransferObjects.Common
 {
     public class EntityEditDto
     {
