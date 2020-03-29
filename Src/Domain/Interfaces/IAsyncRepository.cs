436efb1b8c716d334aed2c05e3b52be13a983245
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-29 21:01:10 +0430

    Add base repository.

---
+++ Src/Domain/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Common;
+
+namespace Domain.Interfaces
+{
+    public interface IAsyncRepository<TEntity> where TEntity : Entity
+    {
+        Task<IEnumerable<TEntity>> ListAllAsync();
+
+        Task<TEntity> GetById(int id);
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+        Task UpdateAsync(TEntity entity);
+
+        Task DeleteAsync(TEntity entity);
+
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Src/Infrastructure/Repositories/AsyncRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Common;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public abstract class AsyncRepository<TEntity> : IAsyncRepository<TEntity> where TEntity : Entity
+    {
+        protected readonly ApplicationDbContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+
+        protected AsyncRepository(ApplicationDbContext db)
+        {
+            Db = db;
+            DbSet = Db.Set<TEntity>();
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> ListAllAsync()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task<TEntity> GetById(int id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public virtual async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await DbSet.AddAsync(entity);
+            await Db.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await Db.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(TEntity entity)
+        {
+            DbSet.Remove(entity);
+            await Db.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(int id)
+        {
+            var entity = await GetById(id);
+            await DeleteAsync(entity);
+        }
+    }
+}
