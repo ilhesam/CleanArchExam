3d2ea04a69a0296d3e9d2e6165e2b98862aeec98
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-30 19:49:58 +0430

    Add post service

---
+++ Src/ApplicationCore/Interfaces/IPostService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.ViewModels.DataTransferObjects.Entities;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IPostService<in TPostAddDto, in TPostEditDto, TPostGetDto> : IAsyncService<TPostAddDto, TPostEditDto, TPostGetDto> where TPostAddDto : PostAddDto where TPostEditDto : PostEditDto where TPostGetDto : PostGetDto
+    {
+    }
+}
