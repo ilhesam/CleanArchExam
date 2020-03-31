f00ead60468bb39708d509f33fc494982fcc95c6
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-31 18:57:12 +0430

    Add post profile.

---
+++ Src/ApplicationCore/Mappers/Profiles/PostProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.ViewModels.DataTransferObjects.Entities;
+using AutoMapper;
+using Domain.Entities;
+
+namespace ApplicationCore.Mappers.Profiles
+{
+    public class PostProfile : Profile
+    {
+        public PostProfile()
+        {
+            CreateMap<Post, PostGetDto>();
+
+            CreateMap<PostAddDto, Post>();
+        }
+    }
+}
