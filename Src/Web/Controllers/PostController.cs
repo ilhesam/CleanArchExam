cea38ba0ade67357d49db09c3df2470a687ca0ee
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-31 19:31:36 +0430

    Add post controller (index action).

---
+++ Src/Web/Controllers/PostController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
+using ApplicationCore.ViewModels.DataTransferObjects;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPostService<PostAddDto, PostEditDto, PostGetDto> _postService;
+
+        public PostController(IPostService<PostAddDto, PostEditDto, PostGetDto> postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var posts = await _postService.ListAllAsync();
+
+            return View(posts);
+        }
+    }
+}
