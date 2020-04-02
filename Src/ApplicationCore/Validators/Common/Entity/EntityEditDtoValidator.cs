c1bf85cbe4985c472015187ef69d13c73a1a02c1
Hesam Asnaashari <ilhesam@outlook.com>
2020-04-02 18:08:06 +0430

    Add dto validators.

---
+++ Src/ApplicationCore/Validators/Common/Entity/EntityEditDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.ViewModels.DataTransferObjects.Common;
+using FluentValidation;
+
+namespace ApplicationCore.Validators.Common
+{
+    public class EntityEditDtoValidator<TEntityEditDto> : AbstractValidator<TEntityEditDto> where TEntityEditDto : EntityEditDto
+    {
+        public EntityEditDtoValidator()
+        {
+        }
+    }
+}
---
+++ Src/ApplicationCore/Validators/Post/PostAddDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.Validators.Common;
+using ApplicationCore.ViewModels.DataTransferObjects;
+using FluentValidation;
+
+namespace ApplicationCore.Validators
+{
+    public class PostAddDtoValidator : EntityAddDtoValidator<PostAddDto>
+    {
+        public PostAddDtoValidator()
+        {
+            RuleFor(p => p.Subject)
+                .NotNull().WithMessage("Subject cannot be null")
+                .NotEmpty().WithMessage("Subject cannot be empty")
+                .MaximumLength(250).WithMessage("Subject cannot be more than 250 characters")
+                .MinimumLength(5).WithMessage("Subject cannot be less than 5 characters");
+
+            RuleFor(p => p.Description)
+                .NotNull().WithMessage("Description cannot be null")
+                .NotEmpty().WithMessage("Description cannot be empty")
+                .MaximumLength(1500).WithMessage("Description cannot be more than 1500 characters")
+                .MinimumLength(30).WithMessage("Description cannot be less than 30 characters");
+
+            RuleFor(p => p.ImageAddress)
+                .NotNull().WithMessage("Image Address cannot be null")
+                .NotEmpty().WithMessage("Image Address cannot be empty")
+                .MaximumLength(1500).WithMessage("Image Address cannot be more than 1500 characters");
+        }
+    }
+}
