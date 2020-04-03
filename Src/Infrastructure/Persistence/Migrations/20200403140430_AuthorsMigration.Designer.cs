29ff9a569aa3b9e1b8740641b4a7d8e05b6b4cc3
Hesam Asnaashari <ilhesam@outlook.com>
2020-04-03 18:36:36 +0430

    Add authors migration.

--- Src/Infrastructure/Persistence/Migrations/20200403140430_AuthorsMigration.Designer.cs
+++ Src/Infrastructure/Persistence/Migrations/20200403140430_AuthorsMigration.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200403140430_AuthorsMigration")]
+    partial class AuthorsMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Src/Infrastructure/Persistence/Migrations/20200403140430_AuthorsMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class AuthorsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    DisplayName = table.Column<string>(maxLength: 250, nullable: false),
+                    RealName = table.Column<string>(maxLength: 250, nullable: false),
+                    EmailAddress = table.Column<string>(maxLength: 250, nullable: false),
+                    Password = table.Column<string>(maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
