d7f5663121601a7c54b10768f76d3a48e1a1ad37
Hesam Asnaashari <ilhesam@outlook.com>
2020-04-03 18:35:30 +0430

    Add author configuration.

---
+++ Src/Infrastructure/Persistence/Configurations/Author/AuthorConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations.Author
+{
+    public class AuthorConfiguration : EntityConfiguration<Domain.Entities.Author>
+    {
+        public override void Configure(EntityTypeBuilder<Domain.Entities.Author> builder)
+        {
+            builder?.Property(a => a.DisplayName)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder?.Property(a => a.RealName)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder?.Property(a => a.EmailAddress)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder?.Property(a => a.Password)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            base.Configure(builder);
+        }
+    }
+}
