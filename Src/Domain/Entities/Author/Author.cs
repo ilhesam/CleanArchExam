b468100ef58b72920b4fbacf845ca78b1ad65d13
Hesam Asnaashari <ilhesam@outlook.com>
2020-04-03 18:28:06 +0430

    Add author entity.

---
+++ Src/Domain/Entities/Author/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Author : Entity
+    {
+        public string DisplayName { get; set; }
+
+        public string RealName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+    }
+}
