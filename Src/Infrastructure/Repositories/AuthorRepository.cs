f03100e1560f14745a46503ac30035407efa269c
Hesam Asnaashari <ilhesam@outlook.com>
2020-04-04 18:07:37 +0430

    Add author repository.

---
+++ Src/Infrastructure/Repositories/AuthorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+
+namespace Infrastructure.Repositories
+{
+    public class AuthorRepository : AsyncRepository<Author>, IAuthorRepository
+    {
+        public AuthorRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
