d92344641c98ef860bb876fec3cfe79bd415e9ef
Hesam Asnaashari <ilhesam@outlook.com>
2020-04-04 18:18:55 +0430

    Add password helper.

---
+++ Src/ApplicationCore/Security/PasswordHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ApplicationCore.Security
+{
+    public static class PasswordHelper
+    {
+        /// <summary>
+        /// Encrypt using MD5
+        /// </summary>
+        /// <param name="password">Password to Encrypt</param>
+        /// <returns>Encrypted Password</returns>
+        public static string EncodePasswordMd5(this string password)   
+        {
+            // Instantiate MD5CryptoServiceProvider, get bytes for original password and compute hash (encoded password)   
+            MD5 md5 = new MD5CryptoServiceProvider();
+            var originalBytes = Encoding.Default.GetBytes(password);
+            var encodedBytes = md5.ComputeHash(originalBytes);
+
+            // Convert encoded bytes back to a 'readable' string   
+            return BitConverter.ToString(encodedBytes);
+        }
+    }
+}
