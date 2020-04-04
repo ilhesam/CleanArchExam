2d66aaa2c1e73396503b3a39b45add9ef904dcf9
Hesam Asnaashari <ilhesam@outlook.com>
2020-04-04 18:14:00 +0430

    Add author profile.

---
+++ Src/ApplicationCore/Mappers/Profiles/AuthorProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.ViewModels.DataTransferObjects;
+using AutoMapper;
+using Domain.Entities;
+
+namespace ApplicationCore.Mappers.Profiles
+{
+    public class AuthorProfile : Profile
+    {
+        public AuthorProfile()
+        {
+            CreateMap<Author, AuthorGetDto>();
+
+            CreateMap<Author, AuthorEditDto>();
+
+            CreateMap<AuthorAddDto, Author>();
+
+            CreateMap<AuthorEditDto, Author>();
+        }
+    }
+}
