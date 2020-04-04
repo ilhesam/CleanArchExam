dad8b4ebcbac6b55f28520cee691f285298ae521
Hesam Asnaashari <ilhesam@outlook.com>
2020-04-04 18:33:27 +0430

    Add password resolver.

---
+++ Src/ApplicationCore/Mappers/Resolvers/PasswordResolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.Security;
+using AutoMapper;
+using Domain.Entities;
+
+namespace ApplicationCore.Mappers.Resolvers
+{
+    public class PasswordResolver<TSource, TDestination> : IValueResolver<TSource, TDestination, string> where TSource : IPassword where TDestination : IPassword
+    {
+        public string Resolve(TSource source, TDestination destination, string destMember, ResolutionContext context)
+            => source.Password.EncodePasswordMd5();
+    }
+}
