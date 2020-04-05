5976ecc8e7a88aacc7b62f8740f564cd054679e9
Hesam Asnaashari <ilhesam@outlook.com>
2020-04-05 19:53:02 +0430

    Add register action to author controller.

---
+++ Src/Web/Controllers/AuthorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
+using ApplicationCore.ViewModels.DataTransferObjects;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IAuthorService<AuthorAddDto, AuthorEditDto, AuthorGetDto> _authorService;
+
+        public AuthorController(IAuthorService<AuthorAddDto, AuthorEditDto, AuthorGetDto> authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register(AuthorAddDto authorAddDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(authorAddDto);
+            }
+
+            await _authorService.AddAsync(authorAddDto);
+
+            return RedirectToAction("");
+        }
+    }
+}
