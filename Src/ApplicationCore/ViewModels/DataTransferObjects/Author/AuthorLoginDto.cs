c970187ba96af62ee2a33ea4d7c44031ba58d23f
Hesam Asnaashari <ilhesam@outlook.com>
2020-04-07 03:41:58 +0430

    Add author login dto.

---
+++ Src/ApplicationCore/ViewModels/DataTransferObjects/Author/AuthorLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.ViewModels.DataTransferObjects
+{
+    public class AuthorLoginDto
+    {
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsPersistent { get; set; } = false;
+    }
+}
